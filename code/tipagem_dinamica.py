019617e3ba8e09dec2debf6e5b59431d06d9bf5c
EliakinCosta <eliakim170@gmail.com>
2017-04-25 03:36:09 -0300

    Apresentacao de Python

---
+++ code/tipagem_dinamica.py
@@ -0,0 +1,6 @@
+a = 2
+
+a = "texto"
+
+#Variaveis funcionam como referencias polimorficas.
+#Verificacao de tipo em runtime. Feita sobre o dado.
---
+++ code/tipagem_estatica.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    int a = 2;
+    String b = "teste";
+    a = b;//ERRADO, FALHA NA COMPILACAO
+  }
+} 
